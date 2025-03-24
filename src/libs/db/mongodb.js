852fe90ad63403ebe509fa6c97a056f5e65d5fef
Anton20HD <anton.haglund@iths.se>
2025-03-24 17:53:58 +0100

    Add dotenv in db file to load env variable for better security

--- src/libs/db/mongodb.js
+++ src/libs/db/mongodb.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined in env");
+}
 
 
 //Dont forget to change to env variable later
